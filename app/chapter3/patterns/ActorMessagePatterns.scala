b18e45683b691cffe4a49e6211669ba35554c81a
Abdul Sattar <asattar@exchangesolutions.com>
2016-10-06 08:06:53 -0400

    ask forward and pipe patterns in terms of tell pattern

--- app/chapter3/patterns/ActorMessagePatterns.scala
+++ app/chapter3/patterns/ActorMessagePatterns.scala
@@ -3,7 +3,7 @@ package chapter3.patterns
 import java.util.concurrent.TimeoutException
 
 import akka.actor.Actor.Receive
-import akka.actor.{Actor, ActorRef, ActorSystem, Props, Status}
+import akka.actor.{Actor, ActorContext, ActorRef, ActorSystem, Props, Status}
 
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{ExecutionContext, Future, Promise}
@@ -37,14 +37,23 @@ class ActorMessagePatterns (actor: ActorRef) {
       actor ! x
 
       context.system.scheduler.scheduleOnce(timeout, self, "timeout")
-    }))
+    }), "tempActor" + Math.random())
 
     p.future
   }
+
+  def forwardMessage(x: Any)(implicit actorCtx: ActorContext) = actor.tell(x, actorCtx sender )
+}
+
+class FuturePipe(f: Future[Any]) {
+  def pipe(actorRef: ActorRef)(implicit executionContext: ExecutionContext) = f onSuccess[Unit](PartialFunction[Any, Unit] (x => {
+    actorRef ! x
+  }))
 }
 
 object ActorMessagePatterns {
 
   implicit val actorMessagePatternAdapter = (actorRef: ActorRef) => new ActorMessagePatterns(actorRef)
+  implicit val futurePipeAdapter = (f: Future[Any]) => new FuturePipe(f)
 
 }
